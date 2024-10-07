0ff1b7503c880dfa0d6b4eb8c5012cc7f31a9892
hBilal-Zia <hbilalzia92@gmail.com>
2024-10-07 13:21:12 +0500

    Configure Port

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
+
+PORT = process.env.PORT
 
 app.get('/', (req, res) =>{
+    // console.log('Endpoint Hit!')
     res.status(200).send({message: "Server is Up"})
 })
 
 
-app.listen(5000, () => {
-    console.log("Server is listening on port: 5000")
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`)
 })
 
